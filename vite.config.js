8b86e80c902ac0a5a8230d5681a1abd5d2c69f0c
wutongshu-6 <2338033876@qq.com>
2026-09-21 23:27:05 +0800

    chore: add GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true,
-    port: 5173,
-  },
+export default defineConfig(({ mode }) => {
+  const isGitHubPages = mode === "pages";
+
+  return {
+    base: isGitHubPages ? "/personal-site/" : "/",
+    build: isGitHubPages ? { outDir: "docs" } : {},
+    plugins: [react()],
+    server: {
+      host: true,
+      port: 5173,
+    },
+  };
 });
